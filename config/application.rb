bb6e9807d258eb8a185956011cfecb82c0eb12f0
Mohith Thimmaiah <thimmaiah@gmail.com>
2015-12-01 08:18:34 -0500

    BAckup

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,4 @@
 require File.expand_path('../boot', __FILE__)
-
 require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
@@ -22,5 +21,16 @@ module LifeOfATrade
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.middleware.use Rack::Cors do
+      allow do
+        origins '*'
+        resource '*',
+        :headers => :any,
+        :expose  => ['access-token', 'expiry', 'token-type', 'uid', 'client'],
+        :methods => [:get, :post, :options, :delete, :put]
+      end
+    end
+
   end
 end
