3eab60e04ed75483d3e229c107472fa771589d1d
Mohith Thimmaiah <thimmaiah@gmail.com>
2015-12-30 11:20:58 +0530

    BAckup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   # protect_from_forgery with: :exception
 
+  rescue_from CanCan::AccessDenied do |exception|
+      render status: 403, template: "/layouts/access_denied.json"
+  end
 end
